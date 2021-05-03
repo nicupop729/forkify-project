e2c9d7d2ae19b4d8ebe94676eebdc4f922773be7
nicupop729 <nicupop729@gmail.com>
2021-05-03 11:07:22 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3ab0d158-74d1-4590-bef3-2be2f2dfd88a';
+export const MODAL_CLOSE_SEC = 2.5;
